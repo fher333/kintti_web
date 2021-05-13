483a5b373e6352a1f558824be4643cfbc2364c13
fernando melendez <fher.melendez@gmail.com>
2021-05-12 19:56:37 -0500

    as

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/kintti_web',{
     useNewUrlParser:true,
     useUnifiedTopology: true,
